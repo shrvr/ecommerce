c4fa9bf25785fafefc02ede2f86bd1a15968c3e6
shrvr <shurvir04@gmail.com>
2022-01-08 17:04:31 -0500

    Added Navbar

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,7 @@
+import { Container } from "../styledComponents/Announcement.style";
+
+const Announcement = () => {
+  return <Container>Super Deal!! Free Shipping on Orders Over $50</Container>;
+};
+
+export default Announcement;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  Container,
+  Wrapper,
+  Left,
+  Language,
+  SearchContainer,
+  Input,
+  Center,
+  Logo,
+  Right,
+  MenuItem,
+} from "../styledComponents/Navbar.style";
+import Badge from "@mui/material/Badge";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <SearchIcon style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
+        </Left>
+
+        <Center>
+          <Logo>LAMA.</Logo>
+        </Center>
+
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>LOGIN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlinedIcon />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
