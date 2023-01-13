287dd8861c363ea6f2d6c4a74b55c76913685c6c
shrvr <shurvir04@gmail.com>
2023-01-13 10:39:27 -0500

    chore(deploy): migrate to render

--- index.js
+++ index.js
@@ -25,7 +25,12 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+if (process.env.NODE_ENV === 'production') {
+  //*Set static folder up in production
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+}
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/client/build", "index.html"));
